467f31e42dfc5a75481b3a1780c0e3eed59e7105
austinngan <angan20@stuy.edu>
2021-01-12 15:46:50 -0500

    finished radixSortSimple

--- RadixSort.java
+++ RadixSort.java
@@ -1,7 +1,7 @@
 public class RadixSort{
 
   public static int nth(int n, int col){
-    return n/ (int)Math.pow(10,col)%10;
+    return n/(int)Math.pow(10,col)%10;
   }
 
   public static int length(int n){
@@ -15,6 +15,40 @@ public class RadixSort{
     for (SortableLinkedList i:buckets){
       original.extend(i);
     }
+  }
+
+  public static void radixSortSimple(SortableLinkedList data){
+    SortableLinkedList[] buckets = new SortableLinkedList[10];
+    SortableLinkedList holder= new SortableLinkedList();
+    int maxDigits=0;
+    for (int i=0;i<10;i++){
+      buckets[i]=new SortableLinkedList();
+    }
+    for (int i=0;i<data.size();i++){
+      if (length(data.get(i))>maxDigits){
+        maxDigits=length(data.get(i));
+      }
+    }
+    for (int y=0;y<maxDigits;y++){
+      for (int i=0;i<data.size();i++){
+        (buckets[nth(data.get(i),y)]).add(data.get(i));
+      }
+      holder.extend(data);
+      merge(data, buckets);
+    }
+  }
+
+  public static void main(String[] args){
+    SortableLinkedList joe=new SortableLinkedList();
+    joe.add(4);
+    joe.add(2);
+    joe.add(7);
+    joe.add(123);
+    joe.add(9);
+    joe.add(234);
+    joe.add(22);
+    radixSortSimple(joe);
+    System.out.println(joe.toString());
   }
 
 }
